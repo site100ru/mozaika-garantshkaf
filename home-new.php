7d15e74acfcd5a700109439b4d811ed86431493f
SlavaWebDeveloper <sidorov-vv3@mail.ru>
2026-06-16 23:46:47 +0500

    подправил главную страницу

--- home-new.php
+++ home-new.php
@@ -99,9 +99,9 @@
 		<!-- Required meta tags --->
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta property="og:title" content="<?php echo $product; ?> и&nbsp;другая корпусная&nbsp;мебель на&nbsp;заказ от&nbsp;производителя<?php echo $location; ?>"/>
-		<meta name="description" content="<?php echo $product; ?> и&nbsp;другая корпусная&nbsp;мебель на&nbsp;заказ от&nbsp;производителя<?php echo $location; ?>"/>
-		<meta property="og:description" content="<?php echo $product; ?> и&nbsp;другая корпусная&nbsp;мебель на&nbsp;заказ от&nbsp;производителя<?php echo $location; ?>"/>
+		<meta property="og:title" content="<?php echo get_bloginfo('name'); ?> - <?php echo get_bloginfo('description'); ?><?php echo $location; ?>"/>
+		<meta name="description" content="<?php echo get_bloginfo('description'); ?><?php echo $location; ?>"/>
+		<meta property="og:description" content="<?php echo get_bloginfo('description'); ?><?php echo $location; ?>"/>
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet" />
@@ -114,7 +114,7 @@
 
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/ico/favicon-light-1.svg" type="image/x-icon" id="favicon">
 
-		<title><?php echo $product; ?> и&nbsp;другая корпусная&nbsp;мебель на&nbsp;заказ от&nbsp;производителя<?php echo $location; ?></title>
+		<title><?php echo get_bloginfo('name'); ?> - <?php echo get_bloginfo('description'); ?><?php echo $location; ?></title>
 
 		<?php if ( $counter_head = get_theme_mod( 'mytheme_counter_head' ) ) : ?>
 			<!-- Код счетчика (head) -->
