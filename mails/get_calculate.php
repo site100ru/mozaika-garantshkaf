d1b2405b19420b2322a4a71443c1e172cde69e18
/usr/bin/vim <support@beget.ru>
2026-09-07 12:47:52 +0300

    Off google recaptcha in get_calculate.php

--- mails/get_calculate.php
+++ mails/get_calculate.php
@@ -3,19 +3,19 @@
 	session_start();
 	$win = "true";
 	
-	// Если существует переменная POST, то
+	/* Если существует переменная POST, то
 	if ( $_POST ) {
 		// Отправляем данные в Google
 		function getCaptcha($SecretKey){
 			$Response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LdV1IcUAAAAABnQ0mXIp5Yh7tLEcAXzdqG6rx9Y&response={$SecretKey}");
 			$Return = json_decode($Response);
 			return $Return;
-		}
+		}*/
 		
-		/* Принимаем данные обратно */
+		/* Принимаем данные обратно
 		$Return = getCaptcha($_POST['g-recaptcha-response']);
 		// Если вероятность робота более 0.5, то считаем отправителя человеком и выполняем отправку почты
-		if ( $Return->success == true && $Return->score > .1 ) {
+		if ( $Return->success == true && $Return->score > .1 ) { */
 	
 			$name = $_POST['name'];
 			$tel = $_POST['tel'];
@@ -115,12 +115,12 @@
 			
 			header("Location: ".$_SERVER['HTTP_REFERER']);
 			
-		} else {
+		/*} else {
 			// Иначе считаем отправителя роботом и выводим сообщение с просьбой повторить попытку
 			$_SESSION['win'] = 1;
 			$_SESSION['recaptcha'] = '<p class="text-light"><strong>Извините!</strong><br>Ваши действия похожи на робота. Пожалуйста повторите попытку!</p>';
 			header("Location: ".$_SERVER['HTTP_REFERER']);
-		}
-	}
+		}*/
+	/*}*/
 	
 ?>
